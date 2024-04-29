4ae5512dd45b794353e2c29db2f1ae5beee69c67
tejas1528 <168460900+tejas1528@users.noreply.github.com>
2024-04-29 17:33:51 +0000

    datatypes

---
+++ ttejas/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+// alret(3 + 3) // we are using node js now browser
+
+let name="tejas"
+let age = 21
+let isMale = true
+
+//number --> 2 to power 53
+//string --> ""
+//boolean --> true or false
+//null --> standalone value
+//undefined -->
+//symbol --> unique
+
+//object -->
+console.log(typeof age)
+console.log(typeof true)
+console.log(typeof null)
+console.log(typeof undefined)
