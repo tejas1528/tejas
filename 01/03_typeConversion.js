e9412dbec850850a90d238cf84781fda79944ec2
tejas1528 <168460900+tejas1528@users.noreply.github.com>
2024-04-30 10:57:34 +0000

    type conversion

---
+++ 01/03_typeConversion.js
@@ -0,0 +1,70 @@
+let age = undefined
+
+// console.log(typeof age)
+// console.log(typeof(age))
+
+let valueInNUmber = Number(age)
+// console.log(typeof valueInNUmber)
+// console.log(valueInNUmber)
+
+/*
+"33" --> 33,type-number
+"33abc" --> Nan(not a number), type-number
+true --> 1,false --> 0, type-number
+*/
+
+
+let isLoggedIn = "t"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+//any number except 0 --> true
+//0 --> false
+//"" -->false
+//"anything in  between" --> true
+
+let numberNum = 22
+
+let stringNum = String(numberNum)
+// console.log(stringNum)
+// console.log(typeof stringNum)
+
+
+// ********** OPERATIONS ************
+
+let value = 4
+let negVal = -value
+// console.log(negVal)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%3)
+
+let str1 = "Hello "
+let str2= "tejas"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("2" + 2)
+// console.log(2 + "2")
+// console.log("2" + 2 + 1)
+// console.log(2 + 2 + "1")
+
+// console.log(true);
+// console.log(+true);
+// console.log(-true);
+// console.log(+false);
+// console.log(+"");
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+let cnt=100
+cnt++
+console.log(cnt);
+
+
