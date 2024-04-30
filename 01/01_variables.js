65cda6daa9e608194abbca2c8e048be6bf8f054d
tejas1528 <168460900+tejas1528@users.noreply.github.com>
2024-04-30 10:57:13 +0000

    variables

---
+++ 01/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail =  "tejas@gmail.com"
+var accountPassword = "12345"
+accountCity = "Ahmedbad"
+let accountState;
+
+// accountId = 1
+accountEmail = "aa@aa.com"
+accountPassword = "54321"
+accountCity = "Chandkheda"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
